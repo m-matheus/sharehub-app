fbdeac7ebad76cc73e9b655ebccf6b5031733164
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-04-30 18:29:19 -0300

    feat: implemented the login page

--- sharehub-frontend/src/components/Login.tsx
+++ sharehub-frontend/src/components/Login.tsx
@@ -1,9 +1,56 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { FcGoogle } from 'react-icons/fc';
+
+
+import logo from '../assets/logowhite.png';
+import sharehubVideo from '../assets/sharehub.mp4';
+
 
 const Login = () => {
+
+    const responseGoogle = (response: any) => {
+        console.log(response);
+    }
+
     return (
-        <div>
-            <h1>Login</h1>
+        <div className='flex justify-start items-center flex-col h-screen'>
+            <div className='relative w-full h-full'>
+                <video 
+                    src={sharehubVideo}
+                    typeof='video/mp4'
+                    loop
+                    controls={false}
+                    muted
+                    autoPlay
+                    className='w-full h-full object-cover'
+                />
+
+                <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+                    <div className='p-5'>
+                        <img src={ logo } width="180px" alt="logo" />
+                    </div>
+
+                    <div className='shadow-2x1'>   
+                        <GoogleLogin 
+                            clientId=''
+                            render={(renderProps) => (
+                                <button 
+                                    type='button'
+                                    className='bg-mainColor flex justify-center items-center p-2 rounded-lg focus:outline-none cursor-pointer text-xs'
+                                    onClick={renderProps.onClick}
+                                    disabled={renderProps.disabled}
+                                >
+                                    <FcGoogle className='mr-4'/> Sign in with Google
+                                </button>    
+                            )}
+                            onSuccess={responseGoogle}
+                            onFailure={responseGoogle}
+                            cookiePolicy='single_host_origin'
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
