5ec3556165537b77222cada32eb5565ded09e7e6
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-05-01 20:58:30 -0300

    refactor: client token
    the token was directly in the client file, was changed to the env file, and referenced as process.env.REACT_APP_SANITY_TOKEN on the client file.

--- sharehub-frontend/src/client.ts
+++ sharehub-frontend/src/client.ts
@@ -6,7 +6,7 @@ export const client = createClient({
   dataset: "production",
   apiVersion: "2021-03-25",
   useCdn: true,
-  token: 'skq7Vn4bOBEkcyOL3O5bBh1S8SqvOoQVmJzm0kze0sfoga1QaQXtwlMSJq8FmBy51CTLDZVgPgdN0F2klCLmSXHGdU9BrmB2K7QIPffmTzBEPvrHJHWHGbQbvdLvW3ubuZt8ZyrfEd2bmysIWwK7mdvBZn9yTg9AMoBxkkf0yPPs6eKVV0E8',
+  token: process.env.REACT_APP_SANITY_TOKEN || undefined,
 });
 
 const builder = imageUrlBuilder(client);
