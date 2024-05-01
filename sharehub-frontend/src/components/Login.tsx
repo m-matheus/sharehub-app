c135f341d8c5f423f6bf0680320de57b57cd326a
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-05-01 20:45:11 -0300

    fix: sign-in response successful

--- sharehub-frontend/src/components/Login.tsx
+++ sharehub-frontend/src/components/Login.tsx
@@ -1,5 +1,6 @@
-import GoogleLogin from 'react-google-login';
-import { FcGoogle } from 'react-icons/fc';
+import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import { jwtDecode } from 'jwt-decode';
+import React from 'react';
 
 import { client } from '../client';
 import { useNavigate } from 'react-router-dom';
@@ -11,62 +12,51 @@ const video = require('../assets/sharehub.mp4');
 const Login = () => {
     const navigate = useNavigate();
 
-    const responseGoogle = (response: any) => {
-        localStorage.setItem('user', JSON.stringify(response.profileObj));
+    const responseGoogle = async (response: any) => {
+        const decoded: { name: string, picture: string, sub: string} = jwtDecode(response.credential);
 
-        const { name, googleId, imageUrl } = response.profileObj;
+        const { name, picture, sub } = decoded;
 
-        const doc = {
-            _id: googleId,
+        const user = {
+            _id: sub,
             _type: 'user',
             userName: name,
-            image: imageUrl
+            image: picture
         }
 
-        client.createIfNotExists(doc).then(() => {
-            navigate('/', { replace: true });
-        })
+        await client.createIfNotExists(user).then(() => {
+            navigate('/');
+        });
+        
     }
 
     return (
-        <div className='flex justify-start items-center flex-col h-screen'>
-            <div className='relative w-full h-full'>
-                <video 
-                    src={ video }
-                    typeof='video/mp4'
-                    loop
-                    controls={false}
-                    muted
-                    autoPlay
-                    className='w-full h-full object-cover'
-                />
-
-                <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
-                    <div className='p-5'>
-                        <img src={ logo } width="180px" alt="logo" />
-                    </div>
-
-                    <div className='shadow-2x1'>   
+        <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN || ''}>
+            <div className='flex justify-start items-center flex-col h-screen'>
+                <div className='relative w-full h-full'>
+                    <video
+                        src={ video }
+                        typeof='video/mp4'
+                        loop
+                        controls={false}
+                        muted
+                        autoPlay
+                        className='w-full h-full object-cover'
+                    />
+                    <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+                        <div className='p-5'>
+                            <img src={ logo } width="180px" alt="logo" />
+                        </div>
+                        <div className='shadow-2x1'>
                         <GoogleLogin 
-                            clientId={process.env.REACT_APP_GOOGLE_API_TOKEN || ''}
-                            render={(renderProps) => (
-                                <button 
-                                    type='button'
-                                    className='bg-mainColor flex justify-center items-center p-2 rounded-lg focus:outline-none cursor-pointer text-xs'
-                                    onClick={renderProps.onClick}
-                                    disabled={renderProps.disabled}
-                                >
-                                    <FcGoogle className='mr-4'/> Sign in with Google
-                                </button>    
-                            )}
-                            onSuccess={responseGoogle}
-                            onFailure={responseGoogle}
-                            cookiePolicy='single_host_origin'
-                        />
+                                    onSuccess={responseGoogle}
+                                    onError={() => console.log('error')}
+                                />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </GoogleOAuthProvider>
     )
 }
 
