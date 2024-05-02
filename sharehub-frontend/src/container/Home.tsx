ecc08ed6f5b075c12278046557b87f59f9116653
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-05-02 19:14:36 -0300

    feat: mobile nav bar

--- sharehub-frontend/src/container/Home.tsx
+++ sharehub-frontend/src/container/Home.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import {HiMenu } from "react-icons/hi";
+import { HiMenu } from "react-icons/hi";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { Link, Route, Routes } from "react-router-dom";
 
@@ -9,38 +9,68 @@ import Pins from "./Pins";
 import { client } from "../client";
 import { userQuery } from "../utils/data";
 
-const logo = require('../assets/logo.png');
-
-
+const logo = require("../assets/logo.png");
 
 const Home = () => {
   const [toggleSidebar, setToggleSidebar] = useState(false);
   const [user, setUser] = useState(null as any);
-  
-  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user') || '{}') : localStorage.clear();
 
+  const scrollRef = useRef(null as any);
+
+  const userInfo =
+    localStorage.getItem("user") !== "undefined"
+      ? JSON.parse(localStorage.getItem("user") || "{}")
+      : localStorage.clear();
 
   useEffect(() => {
     const query = userQuery(userInfo?._id);
-    
+
     client.fetch(query).then((data) => {
       setUser(data[0]);
-    })
+    });
   }, [userInfo?._id]);
-  
+
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className="flex bg-gray-50 md:flex-row flex-col h-screen transaction-height duration-75 ease-out">
       <div className="hidden md:flex h-screen flex-initial">
         <Sidebar />
       </div>
       <div className="flex md:hidden flex-row">
-        <HiMenu fontSize={40} className="cursor-pointer" onClick={() => setToggleSidebar(false)}/>
-        <Link to={"/"}>
-          <img src={logo} alt="logo" className="w-28" />
-        </Link>
-        <Link to={`user-profile/${user?._id}`}>
-          <img src={user?.image} alt="user-profile" className="w-28" />
-        </Link>
+        <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
+          <HiMenu
+            fontSize={40}
+            className="cursor-pointer"
+            onClick={() => setToggleSidebar(true)}
+          />
+          <Link to={"/"}>
+            <img src={logo} alt="logo" className="w-28" />
+          </Link>
+          <Link to={`user-profile/${user?._id}`}>
+            <img src={user?.image} alt="user-profile" className="w-28" />
+          </Link>
+        </div>
+      </div>
+      {toggleSidebar && (
+        <div className="fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
+          <div className="absolute w-full flex justify-end items-center p-2">
+            <AiFillCloseCircle
+              fontSize={30}
+              className="cursor-pointer"
+              onClick={() => setToggleSidebar(false)}
+            />
+          </div>
+          <Sidebar />
+        </div>
+      )}
+      <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+        <Routes>
+          <Route path="user-profile/:userId" element={<UserProfile />} />
+          <Route path="/*" element={<Pins />} />
+        </Routes>
       </div>
     </div>
   );
