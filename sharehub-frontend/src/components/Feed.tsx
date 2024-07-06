675a4a02e4d57613a7d1d95fb9e5cd61f4032379
Matheus Moraes <116031835+m-matheus@users.noreply.github.com>
2024-07-06 11:53:25 -0300

    feat: project finished

--- sharehub-frontend/src/components/Feed.tsx
+++ sharehub-frontend/src/components/Feed.tsx
@@ -4,17 +4,38 @@ import { useParams } from 'react-router-dom';
 import { client } from '../client';
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
+import { feedQuery, searchQuery } from '../utils/data';
 
 const Feed = () => {
-  const [loading, setLoading] = useState(true);
-  
+  const [loading, setLoading] = useState(false);
+  const { categoryId } = useParams();
+  const [pins, setPins] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+
+    if(categoryId) {
+      const query = searchQuery(categoryId);
+
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      })
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+
   if (loading) {
     return <Spinner message="We are adding new ideas to your feed!"/>;
   }
 
   return (
     <div>
-      Feed
+      {pins && <MasonryLayout pins={pins} />}
     </div>
   )
 }
--- sharehub-frontend/src/components/MasonryLayout.tsx
+++ sharehub-frontend/src/components/MasonryLayout.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Masonry from 'react-masonry-css'
+import Pin from './Pin';
 
-const MasonryLayout = () => {
+const breakpointObj = {
+  default: 4,
+  3000: 6,
+  2000: 5,
+  1200: 3,
+  1000: 2,
+  500: 1,
+}
+
+const MasonryLayout = ({ pins }: { pins: any }) => {
   return (
-    <div>
-      MasonryLayout
-    </div>
+    <Masonry className="flex animate-slide-fwd" breakpointCols={breakpointObj}>
+      {pins?.map((pin: any) => ( <Pin key={pin._id} pin={pin} /> ))}
+    </Masonry>
   )
 }
 
